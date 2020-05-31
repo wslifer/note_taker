4a0442be67987fa8b4c669e6243f97f540917bb8
Wesley Slifer <wesleyslifer@ymail.com>
2020-05-31 12:24:29 -0500

    set up express server and added git ignore

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Listening on PORT: ${PORT}`);
+});
