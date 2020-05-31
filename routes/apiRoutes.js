ea1ed7e75c439eee14cd7518bdd0c4ca75b0eef8
Wesley Slifer <wesleyslifer@ymail.com>
2020-05-31 12:40:53 -0500

    added uuid to packagejson and installed. added export func to api routes with get

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,3 +3,14 @@ const fs = require("fs");
 const path = require("path");
 const util = require("util");
 const uuidv1 = require("uuid/v1");
+
+module.exports = function (app) {
+  app.get("/api/notes", (req, res) => {
+    fs.readFile("db/db.json", "utf8", (err, data) => {
+      if (err) {
+        throw err;
+      }
+      res.send(JSON.parse(data));
+    });
+  });
+};
