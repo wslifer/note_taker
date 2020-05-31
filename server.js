94e8a5323a5e590751db81fdbc56461c856c1eff
Wesley Slifer <wesleyslifer@ymail.com>
2020-05-31 13:05:34 -0500

    wrote htmlroute to write files to current htmls

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/api", apiRoutes);
-/* app.use("/", htmlRoutes); */
+app.use("/", htmlRoutes);
 
 app.listen(PORT, (err) => {
   if (err) {
