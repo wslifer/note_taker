cb8c6225549ba3fe524224769f2e2678f53351f8
Wesley Slifer <wesleyslifer@ymail.com>
2020-05-31 13:10:24 -0500

    fixed require statements for htmlroutes and api routes

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,8 +6,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+const apiRoutes = require("./routes/apiRoutes")(app);
+const htmlRoutes = require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, (err) => {
   if (err) {
